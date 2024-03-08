5ef29258cf815f2696dbccab292ca6d1acaea90c
Ayush-Yadav <ayush421301@gmails.com>
2024-03-09 00:10:17 +0530

    Setting Up Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// Config FileSystem, do not change 
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.html',
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
