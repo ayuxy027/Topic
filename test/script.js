f83707d41d3732816dbc8133781a2f9e622cc89f
Ayush Yadav <ayush421301@gmail.com>
2024-05-20 16:15:49 +0530

    Getting Started with Page 2

---
+++ test/script.js
@@ -0,0 +1,10 @@
+function showDesignCards() {
+    document.getElementById("design-cards").style.display = "block";
+    document.getElementById("marketing-cards").style.display = "none";
+  }
+  
+  function showMarketingCards() {
+    document.getElementById("design-cards").style.display = "none";
+    document.getElementById("marketing-cards").style.display = "block";
+  }
+  
